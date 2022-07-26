5a5f26e15a652e1b8549c2923f19dfd270d8e2d9
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-07-26 18:47:15 -0400

    fix analysis

--- packages/stream_video/example/main.dart
+++ packages/stream_video/example/main.dart
@@ -9,7 +9,7 @@ import 'package:stream_video/src/models/user_info.dart';
 //llc demo
 void main(List<String> arguments) async {
   final client = StreamVideoClient('something', logLevel: Level.INFO);
-  final user = UserInfo(id: "whatever");
+  final user = UserInfo(id: "whatever",name: "whatever");
   final token = Token(
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoid2hhdGV2ZXIifQ.ros60dPlc_0hAIc0DbGjlvHYajBevgpUyqCLzvqHB3o");
   await client.setUser(user, token: token);
