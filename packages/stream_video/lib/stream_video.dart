0d314a9dc49691b23e1ec6a6f3a002bd0ad54fbc
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-07-26 17:42:33 -0400

    demo: user credentials

--- packages/stream_video/lib/stream_video.dart
+++ packages/stream_video/lib/stream_video.dart
@@ -1,8 +1,9 @@
 library stream_video;
 
 export 'src/client/client.dart';
+export 'src/core/http/token.dart';
 export 'src/models/models.dart';
+export 'src/models/user_info.dart';
 export 'src/state/controllers/controllers.dart';
 export 'src/video_service/room.dart';
 export 'src/video_service/room_participant.dart';
-export 'src/models/user_info.dart';
--- packages/stream_video_flutter/example/lib/demo_users.dart
+++ packages/stream_video_flutter/example/lib/demo_users.dart
@@ -1,6 +1,31 @@
 import 'package:stream_video_flutter/stream_video_flutter.dart';
 
 class UserCredentials {
-  UserCredentials(this.userInfo);
+  UserCredentials({required this.userInfo, required this.token});
+  final Token token;
   final UserInfo userInfo;
 }
+
+final demoUsers = [
+  UserCredentials(
+      userInfo: UserInfo(
+        id: "alice",
+        name: "Alice",
+      ),
+      token: Token(
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWxpY2UifQ.WZkPaUZb84fLkQoEEFw078Xd1RzwR42XjvBISgM2BAk")),
+  UserCredentials(
+      userInfo: UserInfo(
+        id: "bob",
+        name: "Bob",
+      ),
+      token: Token(
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYm9iIn0.6fqa74FESB2DMUcsIiArBDJR2ckkdSvWiSb7qRLVU6U")),
+  UserCredentials(
+      userInfo: UserInfo(
+        id: "trudy",
+        name: "Trudy",
+      ),
+      token: Token(
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHJ1ZHkifQ.yhwq7Dv7znpFiIZrAb9bOYiEXM_PHtgqoq5pgFeOL78"))
+];
