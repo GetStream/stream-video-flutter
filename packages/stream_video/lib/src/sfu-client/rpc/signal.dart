f0c290fdb780178c34bf8550f21d703596f7e247
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-09-26 16:22:41 +0200

    implement SfuSignalingClient sendCandidate + setPublisher

--- packages/stream_video/lib/src/sfu-client/rpc/signal.dart
+++ packages/stream_video/lib/src/sfu-client/rpc/signal.dart
@@ -1,16 +1,95 @@
+import 'package:http/http.dart';
+import 'package:stream_video/protobuf/video/sfu/sfu_signal_rpc/signal.pb.dart';
+import 'package:stream_video/protobuf/video/sfu/sfu_signal_rpc/signal.pbtwirp.dart';
+import 'package:stream_video/src/core/error/error.dart';
+import 'package:stream_video/src/core/http/token_manager.dart';
+import 'package:stream_video/stream_video.dart';
+import 'package:tart/tart.dart';
 import 'package:webrtc_interface/src/rtc_ice_candidate.dart';
 
-abstract class SfuSignalingClient {
+class SfuSignalingClient {
+  SfuSignalingClient(this.tokenManager) {
+    client = SignalServerProtobufClient('http://localhost:8080', "",
+        hooks: ClientHooks(
+          onRequestPrepared: onClientRequestPrepared,
+        ));
+  }
+  final TokenManager tokenManager;
+  late final SignalServerProtobufClient client;
   //his.sessionId = uuidv4();
-  Future<void> sendCandidate({
+
+  Future<IceCandidateResponse> sendCandidate({
     required bool publisher,
     String? candidate,
     String? sdpMid,
     int? sdpMLineIndex,
     String? usernameFragment,
-  }) ;//async {}
+  }) async {
+    try {
+      final token = await tokenManager.loadToken();
+      final ctx = _withAuth(token);
+      final response = await client.sendIceCandidate(
+          ctx,
+          IceCandidateRequest(
+              publisher: publisher,
+              candidate: candidate,
+              sdpMid: sdpMid,
+              sdpMLineIndex: sdpMLineIndex,
+              usernameFragment: usernameFragment));
 
-  Future<String> setPublisher({String? sdp});// async {}
+      return response;
+    } on TwirpError catch (e) {
+      final method =
+          e.getContext.value(ContextKeys.methodName) ?? 'unknown method';
+      throw StreamVideoError(
+        'Twirp error on method: $method. Code: ${e.getCode}. Message: ${e.getMsg}',
+      );
+    } on InvalidTwirpHeader catch (e) {
+      throw StreamVideoError('InvalidTwirpHeader: $e');
+    } catch (e, stack) {
+      throw StreamVideoError('''
+      Unknown Exception Occurred: $e
+      Stack trace: $stack
+      ''');
+    }
+  }
+
+  /// onClientRequestPrepared is a client hook used to print out the method name of the RPC call
+  Context onClientRequestPrepared(Context ctx, Request req) {
+    final methodNameVal = ctx.value(ContextKeys.methodName);
+    print('RequestPrepared for $methodNameVal');
+    return ctx;
+  }
+
+  Context _withAuth(Token token) {
+    return withHttpRequestHeaders(
+      Context(),
+      {'authorization': 'Bearer ${token.rawValue}'},
+    );
+  }
+
+  Future<SetPublisherResponse> setPublisher({String? sdp}) async {
+    try {
+      final token = await tokenManager.loadToken();
+      final ctx = _withAuth(token);
+      final response =
+          await client.setPublisher(ctx, SetPublisherRequest(sdp: sdp));
+      return response;
+    } on TwirpError catch (e) {
+      final method =
+          e.getContext.value(ContextKeys.methodName) ?? 'unknown method';
+      throw StreamVideoError(
+        'Twirp error on method: $method. Code: ${e.getCode}. Message: ${e.getMsg}',
+      );
+    } on InvalidTwirpHeader catch (e) {
+      throw StreamVideoError('InvalidTwirpHeader: $e');
+    } catch (e, stack) {
+      throw StreamVideoError('''
+      Unknown Exception Occurred: $e
+      Stack trace: $stack
+      ''');
+    }
+  }
 
 // updateAudioMuteState
 // updateVideoMuteState
--- packages/stream_video/lib/src/sfu-client/rtc/publisher.dart
+++ packages/stream_video/lib/src/sfu-client/rtc/publisher.dart
@@ -2,47 +2,45 @@ import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:stream_video/src/sfu-client/rpc/signal.dart';
 
 // class Publisher {
-  // late RTCPeerConnection pc;
+// late RTCPeerConnection pc;
 
-  // Publisher(this.rpcClient);
+// Publisher(this.rpcClient);
 
-  // final SfuSignalingClient rpcClient;
-  Future<RTCPeerConnection> createPublisher(
-    String sfuUrl,
-    SfuSignalingClient rpcClient
-  ) async {
-    final publisher = await createPeerConnection({
-      'iceServers': [
-        {
-          'urls': 'stun:stun.l.google.com:19302',
-        },
-        {
-          'urls': "turn:${sfuUrl}:3478",
-          'username': 'video',
-          'credential': 'video',
-        },
-      ],
-    });
-    publisher.onIceCandidate = (candidate) async {
-      await rpcClient.sendCandidate(
-        publisher: true,
-        candidate: candidate.candidate,
-        sdpMid: candidate.sdpMid,
-        sdpMLineIndex: candidate.sdpMLineIndex,
-        // usernameFragment: candidate.use as String?,
-      );
-    };
-    publisher.onRenegotiationNeeded = () async {
-      final offer = await publisher.createOffer();
-      await publisher.setLocalDescription(offer);
-      final sdp = await rpcClient.setPublisher(
-        sdp: offer.sdp,
-      );
+// final SfuSignalingClient rpcClient;
+Future<RTCPeerConnection> createPublisher(
+    String sfuUrl, SfuSignalingClient rpcClient) async {
+  final publisher = await createPeerConnection({
+    'iceServers': [
+      {
+        'urls': 'stun:stun.l.google.com:19302',
+      },
+      {
+        'urls': "turn:${sfuUrl}:3478",
+        'username': 'video',
+        'credential': 'video',
+      },
+    ],
+  });
+  publisher.onIceCandidate = (candidate) async {
+    await rpcClient.sendCandidate(
+      publisher: true,
+      candidate: candidate.candidate,
+      sdpMid: candidate.sdpMid,
+      sdpMLineIndex: candidate.sdpMLineIndex,
+      // usernameFragment: candidate.use as String?,
+    );
+  };
+  publisher.onRenegotiationNeeded = () async {
+    final offer = await publisher.createOffer();
+    await publisher.setLocalDescription(offer);
+    final sfu = await rpcClient.setPublisher(
+      sdp: offer.sdp,
+    );
 
-      await publisher
-          .setRemoteDescription(RTCSessionDescription(sdp, 'answer'));
-    };
-    return publisher;
-    // pc = publisher;
-  }
+    await publisher
+        .setRemoteDescription(RTCSessionDescription(sfu.sdp, 'answer'));
+  };
+  return publisher;
+  // pc = publisher;
+}
 // }
