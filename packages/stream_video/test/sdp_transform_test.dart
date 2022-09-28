4c99437cab75db16b3fe6daa3d0b1563e034bc88
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2022-09-28 09:33:40 +0200

    cleanup

--- packages/stream_video/test/sdp_transform_test.dart
+++ packages/stream_video/test/sdp_transform_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_string_escapes
+
 import 'dart:convert';
 
 import 'package:stream_video/src/sdp-transform/models.dart';
@@ -8,28 +10,28 @@ import 'package:test/test.dart';
 void main() {
   test('splitAt', () {
     final list = [
-      "v",
-      "o",
-      "s",
-      "t",
-      "c",
-      "a",
-      "a",
-      "a",
-      "m",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "m",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a"
+      'v',
+      'o',
+      's',
+      't',
+      'c',
+      'a',
+      'a',
+      'a',
+      'm',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'm',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a'
     ];
     expect([
       ['v', 'o', 's', 't', 'c', 'a', 'a', 'a'],
@@ -40,35 +42,36 @@ void main() {
 
   test('mediaIndexes', () {
     final list = [
-      "v",
-      "o",
-      "s",
-      "t",
-      "c",
-      "a",
-      "a",
-      "a",
-      "m=audio 54400 RTP/SAVPF 0 96",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "m=video 55400 RTP/SAVPF 97 98",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a",
-      "a"
+      'v',
+      'o',
+      's',
+      't',
+      'c',
+      'a',
+      'a',
+      'a',
+      'm=audio 54400 RTP/SAVPF 0 96',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'm=video 55400 RTP/SAVPF 97 98',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a',
+      'a'
     ];
 
     expect(mediaIndexes(list), [8, 15]);
   });
 
   test('Parse SDP', () {
-    const sdpStr = """v=0\r\n\
+    // ignore: leading_newlines_in_multiline_strings
+    const sdpStr = '''v=0\r\n\
 o=- 20518 0 IN IP4 203.0.113.1\r\n\
 s= \r\n\
 t=0 0\r\n\
@@ -90,10 +93,10 @@ a=rtpmap:98 VP8/90000\r\n\
 a=sendrecv\r\n\
 a=candidate:0 1 UDP 2113667327 203.0.113.1 55400 typ host\r\n\
 a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
-""";
+''';
     final sdp = parseSdp(sdpStr);
 
-    final expected_sdp = Sdp(
+    final expectedSdp = Sdp(
         version: 0,
         origin: Origin(
             username: '-',
@@ -114,68 +117,68 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
         media: [
           Media(
               rtp: [
-                Rtp(payload: 96, codec: "H264", rate: 48000),
-                Rtp(payload: 0, codec: "H264", rate: 8000)
+                Rtp(payload: 96, codec: 'H264', rate: 48000),
+                Rtp(payload: 0, codec: 'H264', rate: 8000)
               ],
-              type: "audio",
+              type: 'audio',
               port: 54400,
-              protocol: "RTP/SAVPF",
-              payloads: "0 96",
-              direction: "sendrecv",
+              protocol: 'RTP/SAVPF',
+              payloads: '0 96',
+              direction: 'sendrecv',
               candidates: [
                 Candidate(
                     foundation: 1,
                     component: 2,
-                    transport: "UDP",
+                    transport: 'UDP',
                     priority: 2113667326,
-                    ip: "203.0.113.1",
+                    ip: '203.0.113.1',
                     port: 54401,
-                    type: "host"),
+                    type: 'host'),
                 Candidate(
                     foundation: 0,
                     component: 1,
-                    transport: "UDP",
+                    transport: 'UDP',
                     priority: 2113667327,
-                    ip: "203.0.113.1",
+                    ip: '203.0.113.1',
                     port: 54400,
-                    type: "host")
+                    type: 'host')
               ]),
           Media(
               rtp: [
-                Rtp(payload: 98, codec: "H264", rate: 90000),
-                Rtp(payload: 97, codec: "H264", rate: 90000)
+                Rtp(payload: 98, codec: 'H264', rate: 90000),
+                Rtp(payload: 97, codec: 'H264', rate: 90000)
               ],
               fmtp: [
                 Fmtp(
                     payload: 97,
-                    config: "profile-level-id=4d0028;packetization-mode=1")
+                    config: 'profile-level-id=4d0028;packetization-mode=1')
               ],
-              type: "video",
+              type: 'video',
               port: 55400,
-              protocol: "RTP/SAVPF",
-              payloads: "97 98",
-              direction: "sendrecv",
+              protocol: 'RTP/SAVPF',
+              payloads: '97 98',
+              direction: 'sendrecv',
               candidates: [
                 Candidate(
                     foundation: 1,
                     component: 2,
-                    transport: "UDP",
+                    transport: 'UDP',
                     priority: 2113667326,
-                    ip: "203.0.113.1",
+                    ip: '203.0.113.1',
                     port: 55401,
-                    type: "host"),
+                    type: 'host'),
                 Candidate(
                     foundation: 0,
                     component: 1,
-                    transport: "UDP",
+                    transport: 'UDP',
                     priority: 2113667327,
-                    ip: "203.0.113.1",
+                    ip: '203.0.113.1',
                     port: 55400,
-                    type: "host")
+                    type: 'host')
               ])
         ]);
 
-    expect(sdp, expected_sdp);
+    expect(sdp, expectedSdp);
   });
 
   test('Parse Origin', () {
@@ -186,7 +189,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
         netType: 'IN',
         ipVer: 4,
         address: '203.0.113.1');
-    String str = "- 20518 0 IN IP4 203.0.113.1";
+    String str = '- 20518 0 IN IP4 203.0.113.1';
     Origin origin = parseOrigin(str);
 
     expect(origin, expected_origin);
@@ -194,7 +197,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
 
   test('Parse Timing', () {
     final expected_timing = Timing(start: 0, stop: 0);
-    String str = "0 0";
+    String str = '0 0';
     Timing timing = parseTiming(str);
 
     expect(timing, expected_timing);
@@ -237,7 +240,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
       direction: direction,
       candidates: candidates,
     );
-    String str = "video 55400 RTP/SAVPF 97 98";
+    String str = 'video 55400 RTP/SAVPF 97 98';
     Media media = parseMedia(str,
         candidates: candidates, fmtps: fmtps, rtps: rtps, direction: direction);
 
@@ -253,7 +256,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
         ip: '203.0.113.1',
         port: 55401,
         type: 'host');
-    String str = "candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host";
+    String str = 'candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host';
     Candidate candidate = parseCandidate(str);
 
     expect(candidate, expected_candidate);
@@ -261,7 +264,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
 
   test('ParseConnection', () {
     final expected_connection = Connection(version: 4, ip: '203.0.113.1');
-    String str = "IN IP4 203.0.113.1";
+    String str = 'IN IP4 203.0.113.1';
     Connection connection = parseConnection(str);
 
     expect(connection, expected_connection);
@@ -272,7 +275,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
         type: 'sha-1',
         hash: '42:89:c5:c6:55:9d:6e:c8:e8:83:55:2a:39:f9:b6:eb:e9:a3:a9:e7');
     String str =
-        "fingerprint:sha-1 42:89:c5:c6:55:9d:6e:c8:e8:83:55:2a:39:f9:b6:eb:e9:a3:a9:e7";
+        'fingerprint:sha-1 42:89:c5:c6:55:9d:6e:c8:e8:83:55:2a:39:f9:b6:eb:e9:a3:a9:e7';
     Fingerprint fingerprint = parseFingerprint(str);
 
     expect(fingerprint, expected_fingerprint);
@@ -282,7 +285,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
     final expected_fmtps = [
       Fmtp(payload: 97, config: 'profile-level-id=4d0028;packetization-mode=1')
     ];
-    String str = "fmtp:97 profile-level-id=4d0028;packetization-mode=1";
+    String str = 'fmtp:97 profile-level-id=4d0028;packetization-mode=1';
     Fmtp fmtp = parseFmtp(str);
     final expected_fmtp = expected_fmtps[0];
     expect(fmtp, expected_fmtp);
@@ -293,7 +296,7 @@ a=candidate:1 2 UDP 2113667326 203.0.113.1 55401 typ host\r\n\
       Rtp(payload: 97, codec: 'H264', rate: 90000),
       Rtp(payload: 98, codec: 'VP8', rate: 90000)
     ];
-    String str = "rtpmap:97 H264/90000";
+    String str = 'rtpmap:97 H264/90000';
     Rtp rtp = parseRtp(str);
     final expected_rtp = expected_rtps[0];
     expect(rtp, expected_rtp);
