7595d42404a4aa2fc9e723ed161be5df1a37b0ee
Deven Joshi <deven9852@gmail.com>
2022-12-15 13:57:37 +0530

    changed StreamVideo implementation

--- dogfooding/lib/main.dart
+++ dogfooding/lib/main.dart
@@ -6,21 +6,20 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:stream_video/stream_video.dart';
 import 'firebase_options.dart';
 
-// TODO: Use DI to scope the client
-final streamVideoClient = StreamVideo(
-  'us83cfwuhy8n', // see <video>/data/fixtures/apps.yaml for API secret
-  coordinatorRpcUrl: //replace with the url obtained with ngrok http 26991
-      'https://rpc-video-coordinator.oregon-v1.stream-io-video.com/rpc',
-  // 'http://192.168.1.7:26991/rpc',
-  coordinatorWsUrl: //replace host with your local ip address
-      'wss://wss-video-coordinator.oregon-v1.stream-io-video.com/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
-  // 'ws://192.168.1.7:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
-);
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  StreamVideo.init(
+    'us83cfwuhy8n', // see <video>/data/fixtures/apps.yaml for API secret
+    coordinatorRpcUrl: //replace with the url obtained with ngrok http 26991
+        'https://rpc-video-coordinator.oregon-v1.stream-io-video.com/rpc',
+    // 'http://192.168.1.7:26991/rpc',
+    coordinatorWsUrl: //replace host with your local ip address
+        'wss://wss-video-coordinator.oregon-v1.stream-io-video.com/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
+    // 'ws://192.168.1.7:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
   );
 
   runApp(const StreamDogFoodingApp());
