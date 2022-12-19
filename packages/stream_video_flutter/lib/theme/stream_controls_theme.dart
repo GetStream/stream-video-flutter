8826ceb154463aec6fe406976d9462133df7cc1e
Leandro Borges Ferreira <lehen01@gmail.com>
2022-12-19 17:53:05 +0100

    StreamControlsTheme sketch

---
+++ packages/stream_video_flutter/lib/theme/stream_controls_theme.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class StreamControlsTheme with Diagnosticable {
+
+  final BoxConstraints constraints;
+
+  StreamControlsTheme(this.constraints);
+}
