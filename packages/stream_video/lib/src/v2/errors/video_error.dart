9b5ff91c6f782ec48a48b68ea6dc9404d6d47603
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2023-01-05 11:20:11 +0100

    Add VideoErrorWithCause

--- packages/stream_video/lib/src/v2/errors/video_error.dart
+++ packages/stream_video/lib/src/v2/errors/video_error.dart
@@ -1,21 +1,26 @@
-/// Represents an SDK error that contains a message and the cause.
+import 'package:meta/meta.dart';
+import 'package:stream_video/src/v2/utils/result.dart';
+
+/// Represents an SDK error that contains a message.
 class VideoError {
-  VideoError({this.message = null, this.cause = null});
+  VideoError({required this.message});
 
   /// The message that represents the error.
-  final String? message;
-
-  /// Cause of the error, either a BE exception or an SDK based one.
-  final Exception? cause;
+  final String message;
 
   static VideoError from(Object? exception) {
     if (exception is String) {
       return VideoError(message: exception);
     } else if (exception is Exception) {
-      return VideoError(cause: exception);
+      return VideoErrorWithCause(cause: exception);
     } else {
-      return VideoError(message: exception?.toString());
+      return VideoError(message: 'Unknown error: ${exception?.toString()}');
     }
+  }
+
+  @internal
+  Future<Result<Never>> asFutureFailureResult() {
+    return Future.value(Failure(this));
   }
 
   @override
@@ -23,14 +28,37 @@ class VideoError {
       identical(this, other) ||
       other is VideoError &&
           runtimeType == other.runtimeType &&
+          message == other.message;
+
+  @override
+  int get hashCode => message.hashCode;
+
+  @override
+  String toString() {
+    return 'VideoError{message: $message}';
+  }
+}
+
+/// Represents an SDK error that contains a message and the cause.
+class VideoErrorWithCause extends VideoError {
+  VideoErrorWithCause({super.message = "", required this.cause});
+
+  /// Cause of the error, either a BE exception or an SDK based one.
+  final Exception cause;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VideoErrorWithCause &&
+          runtimeType == other.runtimeType &&
           message == other.message &&
           cause == other.cause;
 
   @override
-  int get hashCode => message.hashCode ^ cause.hashCode;
+  int get hashCode => super.hashCode ^ cause.hashCode;
 
   @override
   String toString() {
-    return 'VideoError{message: $message, exception: $cause}';
+    return 'VideoErrorWithCause{message: $message, exception: $cause}';
   }
 }
