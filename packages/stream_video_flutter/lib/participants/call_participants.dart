5abba3a25092f73cc9d3a33350c52ee2f09cc677
Dmitrii Bychkov <mail@dbychkov.com>
2023-01-05 06:43:09 +0300

    Add item builder

--- packages/stream_video_flutter/lib/participants/call_participants.dart
+++ packages/stream_video_flutter/lib/participants/call_participants.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:stream_video_flutter/participants/call_participant.dart';
 import 'package:stream_video_flutter/stream_video_flutter.dart';
+
+/// {@template callParticipantWidgetBuilder}
+/// Builder function used to build a participant grid item.
+/// {@endtemplate}
+typedef CallParticipantWidgetBuilder = Widget Function(
+  BuildContext context,
+  int index,
+  Participant participant,
+);
 
 /// Widget that renders all the [StreamCallParticipant], based on the number
 /// of people in a call.
 class StreamCallParticipants extends StatefulWidget {
   const StreamCallParticipants({
     required this.participants,
+    this.itemBuilder,
     Key? key,
   }) : super(key: key);
 
   /// The list of participants to display.
   final List<Participant> participants;
+
+  /// {@macro callParticipantWidgetBuilder}
+  final CallParticipantWidgetBuilder? itemBuilder;
 
   @override
   State<StatefulWidget> createState() {
@@ -26,7 +38,7 @@ class _StreamCallParticipantsState extends State<StreamCallParticipants> {
     final remote = participants.whereType<RemoteParticipant>().toList();
     final local = participants.whereType<LocalParticipant>().toList();
 
-    final participantsToDisplay = [];
+    final List<Participant> participantsToDisplay = [];
     if (local.isEmpty) {
       participantsToDisplay.addAll(remote.take(4).toList());
     } else {
@@ -39,29 +51,17 @@ class _StreamCallParticipantsState extends State<StreamCallParticipants> {
         return StreamCallParticipant(participant: participantsToDisplay[0]);
       case 2:
         return Column(children: [
-          Expanded(
-            child: StreamCallParticipant(participant: participantsToDisplay[0]),
-          ),
-          Expanded(
-            child: StreamCallParticipant(participant: participantsToDisplay[1]),
-          ),
+          Expanded(child: _buildParticipant(participantsToDisplay, 0)),
+          Expanded(child: _buildParticipant(participantsToDisplay, 1)),
         ]);
       case 3:
         return Column(children: [
-          Expanded(
-            child: StreamCallParticipant(participant: participantsToDisplay[0]),
-          ),
+          Expanded(child: _buildParticipant(participantsToDisplay, 0)),
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[1]),
-                ),
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[2]),
-                ),
+                Expanded(child: _buildParticipant(participantsToDisplay, 1)),
+                Expanded(child: _buildParticipant(participantsToDisplay, 2)),
               ],
             ),
           ),
@@ -71,28 +71,16 @@ class _StreamCallParticipantsState extends State<StreamCallParticipants> {
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[0]),
-                ),
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[1]),
-                ),
+                Expanded(child: _buildParticipant(participantsToDisplay, 0)),
+                Expanded(child: _buildParticipant(participantsToDisplay, 1)),
               ],
             ),
           ),
           Expanded(
             child: Row(
               children: [
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[2]),
-                ),
-                Expanded(
-                  child: StreamCallParticipant(
-                      participant: participantsToDisplay[3]),
-                ),
+                Expanded(child: _buildParticipant(participantsToDisplay, 2)),
+                Expanded(child: _buildParticipant(participantsToDisplay, 3)),
               ],
             ),
           ),
@@ -100,5 +88,12 @@ class _StreamCallParticipantsState extends State<StreamCallParticipants> {
       default:
         return Container();
     }
+  }
+
+  Widget _buildParticipant(List<Participant> participants, int index) {
+    final participant = participants[index];
+
+    return widget.itemBuilder?.call(context, index, participant) ??
+        StreamCallParticipant(participant: participant);
   }
 }
