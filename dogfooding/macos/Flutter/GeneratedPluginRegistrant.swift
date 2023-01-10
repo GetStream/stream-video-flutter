a5d25621518a4610d30837c749e2a4479bfe8e7c
Dmitrii Bychkov <mail@dbychkov.com>
2023-01-10 14:47:08 +0300

    Revert sqlite dependencies

--- dogfooding/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dogfooding/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,10 @@ import firebase_auth
 import firebase_core
 import flutter_webrtc
 import path_provider_macos
-import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
