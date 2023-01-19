da75a3c3c2a1756ce9d0e79a2d44ced0d699bc3c
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-01-19 14:48:04 +0100

    Init StreamVideo SDK only if it has not been initialized previously

--- dogfooding/lib/main.dart
+++ dogfooding/lib/main.dart
@@ -12,18 +12,22 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  StreamVideo.init(
-    'us83cfwuhy8n', // see <video>/data/fixtures/apps.yaml for API secret
-    coordinatorRpcUrl: //replace with the url obtained with ngrok http 26991
-        'https://rpc-video-coordinator.oregon-v1.stream-io-video.com/rpc',
-    // 'http://192.168.1.7:26991/rpc',
-    coordinatorWsUrl: //replace host with your local ip address
-        'wss://wss-video-coordinator.oregon-v1.stream-io-video.com/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
-    // 'ws://192.168.1.7:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
-  );
-
+  _initStreamVideo();
   runApp(const StreamDogFoodingApp());
+}
+
+void _initStreamVideo() {
+  if (!StreamVideo.isInitialized()) {
+    StreamVideo.init(
+      'us83cfwuhy8n', // see <video>/data/fixtures/apps.yaml for API secret
+      coordinatorRpcUrl: //replace with the url obtained with ngrok http 26991
+          'https://rpc-video-coordinator.oregon-v1.stream-io-video.com/rpc',
+      // 'http://192.168.1.7:26991/rpc',
+      coordinatorWsUrl: //replace host with your local ip address
+          'wss://wss-video-coordinator.oregon-v1.stream-io-video.com/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
+      // 'ws://192.168.1.7:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
+    );
+  }
 }
 
 class StreamDogFoodingApp extends StatelessWidget {
