f0d54ffc9eb54eb5c5782d992eb22bbf66e58c69
Dmitrii Bychkov <mail@dbychkov.com>
2023-01-27 15:10:00 +0300

    Reorder init steps

--- examples/chat_with_video/chat_with_video_starter/lib/main.dart
+++ examples/chat_with_video/chat_with_video_starter/lib/main.dart
@@ -2,9 +2,9 @@ import 'package:chat_with_video_starter/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  /// TODO: Initialize Stream Video SDK.
-
   /// TODO: Initialize Stream Chat SDK.
+
+  /// TODO: Initialize Stream Video SDK.
 
   runApp(MyApp());
 }
