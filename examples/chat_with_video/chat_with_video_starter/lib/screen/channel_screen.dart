a700a32ec4e5e71c0d149b547aea52cd1d648bbb
Dmitrii Bychkov <mail@dbychkov.com>
2023-01-27 13:17:04 +0300

    Channel screen

--- examples/chat_with_video/chat_with_video_starter/lib/screen/channel_screen.dart
+++ examples/chat_with_video/chat_with_video_starter/lib/screen/channel_screen.dart
@@ -1,10 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
-import 'package:stream_video_flutter/stream_video_flutter.dart';
-
-import 'attachment/call_attachment.dart';
 
 class ChannelScreen extends StatelessWidget {
   const ChannelScreen({
@@ -13,77 +8,17 @@ class ChannelScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: StreamChannelHeader(
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.call_rounded,
-              color: Colors.black,
-            ),
-            onPressed: () async => _startCall(context),
-          ),
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: StreamMessageListView(
-              messageBuilder: (context, details, messages, defaultMessage) {
-                return defaultMessage.copyWith(
-                    customAttachmentBuilders: _customAttachmentBuilders());
-              },
-            ),
-          ),
-          StreamMessageInput(),
-        ],
-      ),
-    );
+    /// Add channel UI here.
+    return SizedBox();
   }
 
   Map<String, AttachmentBuilder> _customAttachmentBuilders() {
-    return {
-      'custom': (context, message, attachments) {
-        return WrapAttachmentWidget(
-          attachmentWidget: CallAttachment(
-            message: message,
-            attachment: attachments.first,
-          ),
-          attachmentShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        );
-      }
-    };
+    /// TODO: Add a builder for call attachments.
+    return {};
   }
 
   /// Creates a new call and sends a message with its metadata to the chat.
   void _startCall(BuildContext context) async {
-    final currentUser = StreamChat.of(context).currentUser;
-    final channel = StreamChannel.of(context).channel;
-
-    final createCallResult = await StreamVideo.instance.createCall(
-      id: 'call${Random().nextInt(10000)}',
-      type: "default",
-      ringing: false,
-      participantIds: [],
-    );
-
-    final call = createCallResult.call;
-
-    channel.sendMessage(
-      Message(
-        attachments: [
-          Attachment(
-            type: "custom",
-            authorName: currentUser?.name ?? "",
-            uploadState: UploadState.success(),
-            extraData: {
-              "callCid": call.callCid,
-            },
-          )
-        ],
-      ),
-    );
+    /// TODO: Add code to start a call.
   }
 }
