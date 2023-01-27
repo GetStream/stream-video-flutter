793037782754141536924f4a6acd431d295793bc
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2023-01-27 12:40:25 +0100

    add discussed changes

--- dogfooding/lib/main.dart
+++ dogfooding/lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:stream_video/stream_video.dart';
 import 'package:stream_video_flutter/stream_video_flutter.dart';
 
 import 'firebase_options.dart';
@@ -14,14 +18,27 @@ Call? initialCall;
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   _initStreamVideo();
+  print('JcLog: PN received in background');
   await _handleRemoteMessage(message);
 }
 
 Future<void> _handleRemoteMessage(RemoteMessage message) async {
   print('JcLog: Handling Remote Message with payload: ${message.data}');
-  await StreamVideo.instance.handlePushNotification(message, (call) {
+  await StreamVideo.instance.handlePushNotification(message, (call) async {
     print('JcLog: on call ($call) accepted, ${message.messageId}');
-    initialCall = call;
+
+    // 1. Application is terminated - storing something in shared preferences and handle in DogfoodingApp
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("callReceived", true);
+    // StreamInstance.consumeActiveCall() - return and clean
+
+    // 2. Application is in the foreground. Let's check if we need to store
+    if (navState.currentContext != null) {
+      // We can know the app is alive
+      // Do navigation
+    }
+
+    // 3. Application is in the bg - simillar to 2.
   });
 }
 
@@ -47,6 +64,7 @@ void _initStreamVideo() {
       // 'ws://192.168.1.7:8989/rpc/stream.video.coordinator.client_v1_rpc.Websocket/Connect',
     );
   }
+  print("JcLog: ${StreamVideo.instance.hashCode}");
 }
 
 class StreamDogFoodingApp extends StatefulWidget {
@@ -59,22 +77,29 @@ class _StreamDogFoodingApp extends State<StreamDogFoodingApp> {
   Future<void> setupInteractedMessage() async {
     // Get any messages which caused the application to open from
     // a terminated state.
-    final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
 
-    print('JcLog: initialMessage: $initialMessage');
+    //TODO: getInitialMessage() will always return null if not FCM notification is clicked. We cannot use it
+    // final initialMessage = await FirebaseMessaging.instance.getInitialMessage();
+    // print('JcLog: initialMessage: $initialMessage');
+    //
+    // // If the message also contains a data property with a "type" of "chat",
+    // // navigate to a chat screen
+    // if (initialMessage != null) {
+    //   _handleMessage(initialMessage);
 
-    // If the message also contains a data property with a "type" of "chat",
-    // navigate to a chat screen
-    if (initialMessage != null) {
-      _handleMessage(initialMessage);
-    }
+    // 1.Application terminated - call StreamInstance.getAcceptedCAll, get a call and navigate
+    final prefs = await SharedPreferences.getInstance();
+    print('JcLog: callReceived: ${prefs.getBool("callReceived")}');
+    await prefs.remove("callReceived");
 
     // Also handle any interaction when the app is in the background via a
     // Stream listener
-    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
-    if (initialCall != null) {
-      print('JcLog: We can navigate to the call screen');
-    }
+    // https://pub.dev/documentation/firebase_messaging/latest/firebase_messaging/FirebaseMessaging/onMessageOpenedApp.html
+    // Only works with FCM notifications!!!!
+    // FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+    // if (initialCall != null) {
+    //   print('JcLog: We can navigate to the call screen');
+    // }
   }
 
   void _handleMessage(RemoteMessage message) {
