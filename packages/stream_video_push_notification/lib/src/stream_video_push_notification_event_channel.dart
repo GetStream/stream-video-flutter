36ec326819db2a84e25dac0c6ac74e40368f9725
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-02-08 10:00:52 +0100

    Create StreamVideoPushNotificationEventChannel

---
+++ packages/stream_video_push_notification/lib/src/stream_video_push_notification_event_channel.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
+
+class StreamVideoPushNotificationEventChannel {
+  const StreamVideoPushNotificationEventChannel(
+      {EventChannel eventChannel =
+          const EventChannel('stream_video_push_notification_events')})
+      : _eventChannel = eventChannel;
+
+  final EventChannel _eventChannel;
+
+  Stream<NativeEvent> get onEvent =>
+      _eventChannel.receiveBroadcastStream().mapNotNull(_parseBroadcast);
+
+  NativeEvent? _parseBroadcast(dynamic data) {
+    if (data is Map) {
+      return NativeEvent(
+        NativeEventType.values.firstWhere((e) => e.name == data['event']),
+        Map<String, dynamic>.from(data['body']),
+      );
+    }
+    return null;
+  }
+}
+
+class NativeEvent {
+  NativeEventType type;
+  dynamic content;
+
+  NativeEvent(this.type, this.content);
+}
+
+enum NativeEventType { ACTION_INCOMING_CALL }
