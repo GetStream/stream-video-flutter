06f28d36483730c5d6ed89f8c6ad7d581f84748e
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-02-08 12:58:17 +0100

    Integrate StreamVideoPushNotification SDK on Dogfooding iOS App

--- dogfooding/ios/Runner/AppDelegate.swift
+++ dogfooding/ios/Runner/AppDelegate.swift
@@ -1,13 +1,40 @@
 import UIKit
 import Flutter
+import PushKit
+import stream_video_push_notification
+import flutter_callkit_incoming
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, PKPushRegistryDelegate {
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+      let mainQueue = DispatchQueue.main
+      let voipRegistry: PKPushRegistry = PKPushRegistry(queue: mainQueue)
+      voipRegistry.delegate = self
+      voipRegistry.desiredPushTypes = [PKPushType.voIP]
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+        print(credentials.token)
+        let deviceToken = credentials.token.map { String(format: "%02x", $0) }.joined()
+        print(deviceToken)
+        SwiftStreamVideoPushNotificationPlugin.setDevicePushTokenVoIP(deviceToken: deviceToken)
+    }
+
+    func pushRegistry(
+        _ registry: PKPushRegistry,
+        didReceiveIncomingPushWith payload: PKPushPayload,
+        for type: PKPushType,
+        completion: @escaping () -> Void
+    ) {
+        let aps = payload.dictionaryPayload["aps"] as? [String: Any]
+        let alert = aps?["alert"] as? [String: Any]
+        let callCid = alert?["call_cid"] as? String
+    }
 }
