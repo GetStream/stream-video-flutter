3ad17a4b8109652953be3ec7de92fa91c3e66522
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-02-20 10:35:01 +0100

    Remove unused class

--- packages/stream_video/lib/src/coordinator/ws/coordinator_socket_listener.dart
+++
@@ -1,7 +0,0 @@
-import 'package:stream_video/src/coordinator/ws/coordinator_events.dart';
-
-abstract class CoordinatorEventListener {
-  /// Used for passing down all [CoordinatorEvent]s coming from
-  /// the WebSocket connection, after parse.
-  void onCoordinatorEvent(CoordinatorEvent event);
-}
