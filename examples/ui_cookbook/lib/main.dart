4c710bd9777bcc7a8311d02c698da042a4ef34f7
Rafal Adasiewicz <rafal@getstream.io>
2023-03-15 18:43:29 +0100

    Add Participant List To UI Cookbook (#282)

---
+++ examples/ui_cookbook/lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:stream_video/stream_video.dart';
+
+import 'env/env.dart';
+import 'participant_list.dart';
+
+void main() async {
+  /// Initialize Stream Video SDK.
+  StreamVideo.init(
+    Env.streamVideoApiKey,
+    logLevel: LogLevel.info,
+  );
+
+  await StreamVideo.instance.connectUser(
+    const UserInfo(
+      id: Env.sampleUserId00,
+      role: Env.sampleUserRole00,
+      name: Env.sampleUserName00,
+      imageUrl: Env.sampleUserImage00,
+    ),
+    token: Token(Env.sampleUserVideoToken00),
+  );
+
+  runApp(const UICookbook());
+}
+
+class UICookbook extends StatelessWidget {
+  const UICookbook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UI Cookbook',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Cookbook'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          OptionButton(
+            onPressed: () async {
+              final call =
+                  await generateCall('default', generateAlphanumericString(10));
+
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) {
+                    return ParticipantListScreen(
+                      call: call,
+                    );
+                  },
+                ),
+              );
+            },
+            label: 'Participant List',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OptionButton extends StatelessWidget {
+  const OptionButton({super.key, required this.onPressed, required this.label});
+
+  final VoidCallback onPressed;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(label),
+      ),
+    );
+  }
+}
+
+const _chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
+Random _rnd = Random();
+
+Future<Call> generateCall(String type, String id) async {
+  final callCid = StreamCallCid.from(
+    type: type,
+    id: id,
+  );
+  final data = await StreamVideo.instance.getOrCreateCall(cid: callCid);
+  final call = Call.fromCreated(data: data.getDataOrNull()!.data);
+
+  return call;
+}
+
+String generateAlphanumericString(int length) => String.fromCharCodes(
+      Iterable.generate(
+        length,
+        (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length)),
+      ),
+    );
