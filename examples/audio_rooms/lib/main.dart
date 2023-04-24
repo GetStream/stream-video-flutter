049008666e88c6c8d6cb04a72ddd4428471a1029
Nash <nashville@surfer-172-29-13-53-hotspot.s-bit.nl>
2023-04-25 00:46:30 +0200

    Replace home route with login screen

--- examples/audio_rooms/lib/main.dart
+++ examples/audio_rooms/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:audio_rooms/screens/audio_room_screen.dart';
+import 'package:audio_rooms/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -47,7 +47,7 @@ class AudioRooms extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFDDDDDD),
         primaryColor: const Color(0xFF005FFF),
       ),
-      home: const AudioRoomScreen(),
+      home: const LoginScreen(),
     );
   }
 }
