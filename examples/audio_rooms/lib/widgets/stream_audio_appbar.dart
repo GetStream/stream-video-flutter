872c329aa7de25d084ee40dc2fce1d22c9174ab5
Nash <nashville@surfer-172-29-13-53-hotspot.s-bit.nl>
2023-04-25 00:45:16 +0200

    Add settings and signout button to appbar

--- examples/audio_rooms/lib/widgets/stream_audio_appbar.dart
+++ examples/audio_rooms/lib/widgets/stream_audio_appbar.dart
@@ -1,20 +1,46 @@
 import 'package:audio_rooms/assets.dart';
+import 'package:audio_rooms/widgets/stream_button.dart';
 import 'package:flutter/material.dart';
 
-class StreamAudioRoomsAppBar extends StatelessWidget implements PreferredSizeWidget {
+class StreamAudioRoomsAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
   const StreamAudioRoomsAppBar({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      elevation: 0.0,
-      backgroundColor: const Color(0xFFDDDDDD),
-      leading: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: CircleAvatar(
-          backgroundImage: AssetImage(kStreamAudioIconAsset),
+    final mediaQuery = MediaQuery.of(context);
+    return Container(
+      color: const Color(0xFFDDDDDD),
+      child: Padding(
+        padding: EdgeInsets.only(
+          top: mediaQuery.viewPadding.top + 8.0,
+          left: mediaQuery.viewPadding.left + 8.0,
+          right: mediaQuery.viewPadding.right + 8.0,
+        ),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage(kStreamAudioIconAsset),
+            ),
+            const Spacer(),
+            const Icon(
+              Icons.settings,
+              size: 34,
+              color: Color(0xFF4C525C),
+            ),
+            const SizedBox(width: 24),
+            StreamButton(
+              height: 18,
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                "Sign out",
+              ),
+            ),
+          ],
         ),
       ),
     );
