31186176d72ed2e2f99770c7b2b0a86926ff9050
Neevash Ramdial <mail@neevash.dev>
2023-05-30 15:43:59 -0600

    Move examples to dedicate sample repo

--- examples/chat_with_video/chat_with_video_starter/android/app/src/main/kotlin/io/getstream/chat_with_video_starter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.getstream.chat_with_video_starter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/video_with_chat/android/app/src/main/kotlin/io/getstream/video_with_chat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.getstream.video_with_chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
