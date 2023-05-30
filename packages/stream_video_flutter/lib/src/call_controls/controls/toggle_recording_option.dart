599a2201a96fd1d1a29d0b9052c166d4f81bd259
Deven Joshi <deven9852@gmail.com>
2023-05-30 16:16:06 +0530

    add toggle recording option

---
+++ packages/stream_video_flutter/lib/src/call_controls/controls/toggle_recording_option.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../../stream_video_flutter.dart';
+
+/// A widget that represents a call control option to toggle if recording
+/// is on or off.
+class ToggleRecordingOption extends StatelessWidget {
+  /// Creates a new instance of [ToggleRecordingOption].
+  const ToggleRecordingOption({
+    super.key,
+    required this.call,
+    this.enabledRecordingIcon = Icons.video_file_outlined,
+    this.disabledRecordingIcon = Icons.video_file_rounded,
+  });
+
+  /// Represents a call.
+  final Call call;
+
+  /// The icon that is shown when the recording is enabled.
+  final IconData enabledRecordingIcon;
+
+  /// The icon that is shown when the recording is disabled.
+  final IconData disabledRecordingIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    final enabled = call.state.value.isRecording;
+
+    return CallControlOption(
+      icon:
+      enabled ? Icon(enabledRecordingIcon) : Icon(disabledRecordingIcon),
+      onPressed: () {
+        if(!enabled) {
+          call.goLive();
+        } else {
+          call.stopLive();
+        }
+      },
+    );
+  }
+}
