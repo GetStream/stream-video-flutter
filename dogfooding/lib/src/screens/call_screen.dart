2e18dc0fff8f2c9df6f24a177a06488cfecfec0e
Neevash Ramdial <mail@neevash.dev>
2023-07-05 12:38:00 -0600

    quietly leave call

--- dogfooding/lib/src/screens/call_screen.dart
+++ dogfooding/lib/src/screens/call_screen.dart
@@ -82,7 +82,6 @@ class _CallScreenState extends State<CallScreen> {
                       LeaveCallOption(
                         call: call,
                         onLeaveCallTap: () {
-                          call.end();
                           call.disconnect();
                         },
                       ),
