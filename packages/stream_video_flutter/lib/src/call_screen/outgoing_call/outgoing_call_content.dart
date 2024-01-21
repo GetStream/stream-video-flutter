9c792165b3283ea6d4a6f9b108c3aeac2abc913d
Maor Meir Hajaj <maor@intune.ltd>
2024-01-21 13:12:42 +0200

    Add to `StreamOutgoingCallContent` optional widget as background Fixes #573

--- packages/stream_video_flutter/lib/src/call_screen/outgoing_call/outgoing_call_content.dart
+++ packages/stream_video_flutter/lib/src/call_screen/outgoing_call/outgoing_call_content.dart
@@ -7,6 +7,13 @@ import '../common/calling_participants.dart';
 import '../common/participant_avatars.dart';
 import 'outgoing_call_controls.dart';
 
+typedef OutgoingCallBackground = Widget Function(
+  Call call,
+  CallState callState,
+  List<UserInfo> participants,
+  Widget child,
+);
+
 /// Represents the Outgoing Call state and UI, when the user is calling
 /// other people.
 class StreamOutgoingCallContent extends StatefulWidget {
@@ -15,6 +22,7 @@ class StreamOutgoingCallContent extends StatefulWidget {
     super.key,
     required this.call,
     required this.callState,
+    this.backgroundWidget,
     this.onCancelCallTap,
     this.onMicrophoneTap,
     this.onCameraTap,
@@ -63,6 +71,11 @@ class StreamOutgoingCallContent extends StatefulWidget {
   /// Builder used to create a custom widget for participants display names.
   final ParticipantsDisplayNameBuilder? participantsDisplayNameBuilder;
 
+  /// A widget that is placed behind the outgoing call UI instead of the default
+  ///
+  /// background. Preferably use a [Stack] widget, like in the default [CallBackground].
+  final OutgoingCallBackground? backgroundWidget;
+
   @override
   State<StreamOutgoingCallContent> createState() =>
       _StreamOutgoingCallContentState();
@@ -90,9 +103,7 @@ class _StreamOutgoingCallContentState extends State<StreamOutgoingCallContent> {
     final participants =
         widget.callState.otherParticipants.map((e) => e.toUserInfo()).toList();
 
-    return CallBackground(
-      participants: participants,
-      child: Material(
+    final child = Material(
         color: Colors.transparent,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -139,7 +150,17 @@ class _StreamOutgoingCallContentState extends State<StreamOutgoingCallContent> {
             ),
           ],
         ),
-      ),
+    );
+
+    return widget.backgroundWidget?.call(
+          widget.call,
+          widget.callState,
+          participants,
+          child,
+        ) ??
+        CallBackground(
+          participants: participants,
+          child: child,
     );
   }
 
