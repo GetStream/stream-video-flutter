9acfa8a420502bcdffd8febe8f4cab6718255f64
Maciej Brażewicz <maciej.brazewicz@getstream.io>
2024-08-02 15:51:45 +0200

    removed previously deprecated paramteres (#726)

--- packages/stream_video_flutter/lib/src/call_participants/local_video.dart
+++ packages/stream_video_flutter/lib/src/call_participants/local_video.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../../stream_video_flutter.dart';
-import '../widgets/floating_view/floating_view_alignment.dart';
 
 /// Represents a floating item used to feature a participant video.
 class StreamLocalVideo extends StatelessWidget {
