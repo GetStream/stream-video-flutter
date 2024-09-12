5b037a60bff0663354565848483b748c0536fb43
Maciej Brażewicz <maciej.brazewicz@getstream.io>
2024-09-12 10:16:17 +0200

    fix(llc): rewrite of the rtc video layers creation logic - fix for video quality issues (#754)
    * rewrite of the rtc video layers creation logic
    
    * typo fix

--- packages/stream_video/lib/src/webrtc/codecs_helper.dart
+++ packages/stream_video/lib/src/webrtc/codecs_helper.dart
@@ -1,97 +1,114 @@
-import 'dart:math' as math;
+// import 'dart:math' as math;
 
+import 'dart:math';
+
+import 'package:collection/collection.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
 
-import '../logger/stream_log.dart';
 import 'model/rtc_video_dimension.dart';
 import 'model/rtc_video_parameters.dart';
 
-// 16:9 default
-const _defaultSimulcast_16x9 = {
-  'f': RtcVideoParametersPresets.h720_16x9,
-  'h': RtcVideoParametersPresets.h360_16x9,
-  'q': RtcVideoParametersPresets.h180_16x9,
-};
-
-// 4:3 default
-const _defaultSimulcast_4x3 = {
-  'f': RtcVideoParametersPresets.h720_4x3,
-  'h': RtcVideoParametersPresets.h360_4x3,
-  'q': RtcVideoParametersPresets.h180_4x3,
-};
-
-List<rtc.RTCRtpEncoding> computeVideoEncodings({
-  required RtcVideoDimension dimension,
-  required bool isScreenShare,
+List<rtc.RTCRtpEncoding> findOptimalVideoLayers({
+  required RtcVideoDimension dimensions,
+  RtcVideoParameters targetResolution = RtcVideoParametersPresets.h720_16x9,
 }) {
-  final presets = _presetsForDimension(
-    isScreenShare: isScreenShare,
-    dimension: dimension,
+  final optimalVideoLayers = <rtc.RTCRtpEncoding>[];
+
+  final maxBitrate = getComputedMaxBitrate(
+    targetResolution,
+    dimensions.width,
+    dimensions.height,
   );
-  presets.forEach((rid, preset) {
-    streamLog.v(
-      'SV:RtcManager',
-      () => '[publishVideoTrack] #$rid; preset: $preset',
-    );
-  });
 
-  return encodingsFromPresets(dimension, presets: presets);
-}
-
-Map<String, RtcVideoParameters> _presetsForDimension({
-  required bool isScreenShare,
-  required RtcVideoDimension dimension,
-}) {
-  if (isScreenShare) {
-    return _defaultSimulcast_16x9;
-  }
-
-  final aspectRatio = dimension.aspect();
-  streamLog.v(
-    'SV:RtcManager',
-    () => '[publishVideoTrack] aspectRatio: $aspectRatio',
-  );
-  if (_is16x9ratio(aspectRatio)) {
-    streamLog.v(
-      'SV:RtcManager',
-      () => '[publishVideoTrack] defaultSimulcast_16x9',
-    );
-    return _defaultSimulcast_16x9;
-  }
-  streamLog.v(
-    'SV:RtcManager',
-    () => '[publishVideoTrack] defaultSimulcast_4x3',
-  );
-  return _defaultSimulcast_4x3;
-}
-
-List<rtc.RTCRtpEncoding> encodingsFromPresets(
-  RtcVideoDimension dimension, {
-  required Map<String, RtcVideoParameters> presets,
-}) {
-  final result = <rtc.RTCRtpEncoding>[];
-
-  presets.forEach((rid, preset) {
-    final double scaleResolutionDownBy =
-        math.max(1, dimension.min() / preset.dimension.min());
-    streamLog.v(
-      'SV:RtcManager',
-      () => '[publishVideoTrack] #$rid; scaleResolutionDownBy: '
-          '$scaleResolutionDownBy',
-    );
-    result.add(
+  var downscaleFactor = 1;
+  for (final rid in ['f', 'h', 'q'].reversed) {
+    optimalVideoLayers.insert(
+      0,
       rtc.RTCRtpEncoding(
         rid: rid,
-        scaleResolutionDownBy: scaleResolutionDownBy,
-        maxFramerate: preset.encoding.maxFramerate,
-        maxBitrate: preset.encoding.maxBitrate,
+        scaleResolutionDownBy: downscaleFactor.toDouble(),
+        maxFramerate: 30,
+        maxBitrate: (maxBitrate / downscaleFactor).round(),
       ),
     );
-  });
-  return result;
+
+    downscaleFactor *= 2;
+  }
+
+  return withSimulcastConstraints(
+    dimensions: dimensions,
+    optimalVideoLayers: optimalVideoLayers,
+  );
 }
 
-bool _is16x9ratio(double aspectRatio) {
-  return (aspectRatio - RtcVideoDimensionHelpers.aspect_16x9).abs() <
-      (aspectRatio - RtcVideoDimensionHelpers.aspect_4x3).abs();
+int getComputedMaxBitrate(
+  RtcVideoParameters targetResolution,
+  int currentWidth,
+  int currentHeight,
+) {
+  // if the current resolution is lower than the target resolution,
+  // we want to proportionally reduce the target bitrate
+  final targetWidth = targetResolution.dimension.width;
+  final targetHeight = targetResolution.dimension.height;
+
+  if (currentWidth < targetWidth || currentHeight < targetHeight) {
+    final currentPixels = currentWidth * currentHeight;
+    final targetPixels = targetWidth * targetHeight;
+    final reductionFactor = currentPixels / targetPixels;
+
+    return (targetResolution.encoding.maxBitrate * reductionFactor).round();
+  }
+
+  return targetResolution.encoding.maxBitrate;
+}
+
+List<rtc.RTCRtpEncoding> withSimulcastConstraints({
+  required RtcVideoDimension dimensions,
+  required List<rtc.RTCRtpEncoding> optimalVideoLayers,
+}) {
+  var layers = <rtc.RTCRtpEncoding>[];
+
+  final size = max(dimensions.width, dimensions.height);
+  if (size <= 320) {
+    // provide only one layer 320x240 (q), the one with the highest quality
+    layers = optimalVideoLayers.where((layer) => layer.rid == 'f').toList();
+  } else if (size <= 640) {
+    // provide two layers, 160x120 (q) and 640x480 (h)
+    layers = optimalVideoLayers.where((layer) => layer.rid != 'h').toList();
+  } else {
+    // provide three layers for sizes > 640x480
+    layers = optimalVideoLayers;
+  }
+
+  final ridMapping = ['q', 'h', 'f'];
+  return layers
+      .mapIndexed(
+        (index, layer) => rtc.RTCRtpEncoding(
+          rid: ridMapping[index],
+          scaleResolutionDownBy: layer.scaleResolutionDownBy,
+          maxFramerate: layer.maxFramerate,
+          maxBitrate: layer.maxBitrate,
+        ),
+      )
+      .toList();
+}
+
+List<rtc.RTCRtpEncoding> findOptimalScreenSharingLayers({
+  required RtcVideoDimension dimensions,
+  RtcVideoParameters targetResolution = RtcVideoParametersPresets.h1080_16x9,
+}) {
+  final optimalVideoLayers = <rtc.RTCRtpEncoding>[];
+
+  for (final rid in ['f', 'h', 'q'].reversed) {
+    optimalVideoLayers.insert(
+      0,
+      rtc.RTCRtpEncoding(
+        rid: rid,
+        maxFramerate: targetResolution.encoding.maxFramerate,
+        maxBitrate: targetResolution.encoding.maxBitrate,
+      ),
+    );
+  }
+
+  return optimalVideoLayers;
 }
