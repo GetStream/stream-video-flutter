6e89cdb093e3bbead984818b74b537fed016d046
Rene Floor <rene.floor@getstream.io>
2025-02-19 14:11:07 +0100

    Chore: setup build flavors for android and iOS
    test options

--- dogfooding/lib/utils/consts.dart
+++ dogfooding/lib/utils/consts.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/services.dart' show appFlavor;
 import 'package:stream_video_flutter/stream_video_flutter.dart';
 
 final StreamCallType kCallType = StreamCallType.defaultType();
 const String kMessageChannelType = 'videocall';
 const String kAppName = 'Stream Dogfooding';
 const double kMaxWidthRegularScreen = 500;
+bool get kIsProd => switch (appFlavor) {
+      'dev' => false,
+      'beta' => false,
+      'prod' => true,
+      _ => throw ArgumentError(
+          'Flavors are required. Supported are dev, beta and prod'),
+    };
