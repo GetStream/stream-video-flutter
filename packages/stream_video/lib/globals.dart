6786e66c4d014c40902cfc95fbda567afaee3f8b
Brazol <brazol@gmail.com>
2025-02-27 09:14:40 +0100

    changed sdkIdentifier

--- packages/stream_video/lib/globals.dart
+++ packages/stream_video/lib/globals.dart
@@ -5,7 +5,8 @@ const String streamVideoVersion = '0.8.0';
 const String androidWebRTCVersion = 'libwebrtc-m125.6422.03';
 const String iosWebRTCVersion = 'libwebrtc-m125.6422.06';
 
-const String streamDefaultUserAgent = 'stream-video-dart-v$streamVideoVersion';
+const String streamDefaultUserAgent =
+    'stream-video-flutter-v$streamVideoVersion';
 final xStreamClientHeader = '$streamDefaultUserAgent|$clientVersionDetails';
 
 @internal
