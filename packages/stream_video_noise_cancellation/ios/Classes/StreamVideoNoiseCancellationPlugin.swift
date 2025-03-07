6ad7c312cfff12ec12885016901d84ec65be56a7
Brazol <brazol@gmail.com>
2025-03-07 10:59:06 +0100

    iOS Noise cancellation implementation addded

--- packages/stream_video_noise_cancellation/ios/Classes/StreamVideoNoiseCancellationPlugin.swift
+++ packages/stream_video_noise_cancellation/ios/Classes/StreamVideoNoiseCancellationPlugin.swift
@@ -1,17 +1,66 @@
 import Flutter
+import StreamVideoNoiseCancellation
 import UIKit
+import stream_webrtc_flutter
 
 public class StreamVideoNoiseCancellationPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "stream_video_noise_cancellation", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(
+      name: "stream_video_noise_cancellation", binaryMessenger: registrar.messenger())
     let instance = StreamVideoNoiseCancellationPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
+  let processingModule = StreamAudioFilterProcessingModule()
+  var noiseCancellationFilter: NoiseCancellationFilter!
+  var noiseCancellationProcessor: NoiseCancellationProcessor!
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "registerProcessor":
+      noiseCancellationProcessor = NoiseCancellationProcessor()
+      noiseCancellationFilter = NoiseCancellationFilter(
+        name: "noise-cancellation",
+        initialize: noiseCancellationProcessor.initialize,
+        process: noiseCancellationProcessor.process,
+        release: noiseCancellationProcessor.release)
+
+      AudioManager.sharedInstance().audioProcessingModule = processingModule.processingModule
+
+      result(nil)
+    case "isEnabled":
+      result(processingModule.activeAudioFilterId != nil)
+    case "setEnabled":
+      if noiseCancellationFilter == nil {
+        result(
+          FlutterError(
+            code: "NOISE_CANCELLATION_FILTER_NOT_REGISTERED",
+            message: "Noise cancellation filter not registered",
+            details: nil)
+        )
+        return
+      }
+
+      if let arguments = call.arguments as? [String: Any] {
+        guard let enabled = arguments["enabled"] as? Bool
+        else {
+          result(
+            FlutterError(
+              code: "INVALID_ARGUMENT", message: "Invalid argument", details: nil)
+          )
+          return
+        }
+
+        if enabled {
+          processingModule.setAudioFilter(noiseCancellationFilter)
+        } else {
+          processingModule.setAudioFilter(nil)
+        }
+      }
+
+      result(nil)
+    case "deviceSupportsAdvancedAudioProcessing":
+      result(neuralEngineExists)
     default:
       result(FlutterMethodNotImplemented)
     }
--- packages/stream_video_noise_cancellation/ios/stream_video_noise_cancellation.podspec
+++ packages/stream_video_noise_cancellation/ios/stream_video_noise_cancellation.podspec
@@ -4,7 +4,7 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'stream_video_noise_cancellation'
-  s.version          = '0.0.1'
+  s.version          = '0.8.3'
   s.summary          = 'Official Noise Cancellation Plugin for Stream Video.'
   s.description      = <<-DESC
 Official Noise Cancellation Plugin for Stream Video.
@@ -15,15 +15,12 @@ Official Noise Cancellation Plugin for Stream Video.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '12.0'
+  s.dependency 'stream_webrtc_flutter'
+  s.dependency 'StreamVideoNoiseCancellation'
+  s.platform = :ios, '14.0'
+  s.static_framework = true
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
-
-  # If your plugin requires a privacy manifest, for example if it uses any
-  # required reason APIs, update the PrivacyInfo.xcprivacy file to describe your
-  # plugin's privacy impact, and then uncomment this line. For more information,
-  # see https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-  # s.resource_bundles = {'stream_video_noise_cancellation_privacy' => ['Resources/PrivacyInfo.xcprivacy']}
 end
