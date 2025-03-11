b3db55987ec32437866da387e39d108b5b54bbdc
Rene Floor <rene.floor@getstream.io>
2025-03-11 13:21:56 +0100

    dogfooding: allow env switching on web

--- dogfooding/lib/utils/consts.dart
+++ dogfooding/lib/utils/consts.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' show appFlavor;
 import 'package:stream_video_flutter/stream_video_flutter.dart';
 
@@ -9,5 +10,5 @@ bool get kIsProd => switch (appFlavor) {
       'dev' => false,
       'beta' => false,
       'prod' => true,
-      _ => true,
+      _ => !kIsWeb, // On our web builds we enable non-prod features.
     };
