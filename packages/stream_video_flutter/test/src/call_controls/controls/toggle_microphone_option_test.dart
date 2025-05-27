f4410bb3c6b631b4fe9e65ed1b999c94330efc67
Rene Floor <rene.floor@getstream.io>
2025-05-27 14:34:19 +0200

    chore: also test disabled microphone

--- packages/stream_video_flutter/test/src/call_controls/controls/toggle_microphone_option_test.dart
+++ packages/stream_video_flutter/test/src/call_controls/controls/toggle_microphone_option_test.dart
@@ -7,15 +7,22 @@ import '../../../test_utils/test_wrapper.dart';
 
 void main() {
   testWidgets('ToggleMicrophoneOption', (tester) async {
+    var isAudioEnabled = true;
+
     final localParticipant = MockCallParticipantState();
     final call = MockCall();
 
-    when(() => localParticipant.isAudioEnabled).thenReturn(true);
+    when(() => localParticipant.isAudioEnabled).thenReturn(isAudioEnabled);
     when(() => call.setMicrophoneEnabled(enabled: any(named: 'enabled')))
         .thenAnswer(
-      (_) async => const Result.success(none),
+      (invocation) async {
+        isAudioEnabled =
+            invocation.namedArguments[const Symbol('enabled')] as bool;
+        return const Result.success(none);
+      },
     );
 
+    // Microphone is enabled
     await tester.pumpWidget(
       TestWrapper(
         child: ToggleMicrophoneOption(
@@ -32,6 +39,19 @@ void main() {
     await tester.pumpAndSettle();
 
     verify(() => call.setMicrophoneEnabled(enabled: false)).called(1);
+
+    // Microphone is disabled
+    when(() => localParticipant.isAudioEnabled).thenReturn(isAudioEnabled);
+    await tester.pumpWidget(
+      TestWrapper(
+        child: ToggleMicrophoneOption(
+          localParticipant: localParticipant,
+          call: call,
+        ),
+      ),
+    );
+    expect(find.byIcon(Icons.mic_rounded), findsNothing);
+    expect(find.byIcon(Icons.mic_off_rounded), findsOneWidget);
   });
 }
 
