6e34161dca2c97709ba8d38b5064dad5d721c0e8
Rene Floor <rene.floor@getstream.io>
2025-07-04 16:01:38 +0200

    repackaged webrtc used

--- dogfooding/lib/di/injector.dart
+++ dogfooding/lib/di/injector.dart
@@ -9,7 +9,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart' hide User;
 import 'package:stream_video_flutter/stream_video_flutter.dart';
-import 'package:stream_video_noise_cancellation/noise_cancellation_audio_processor.dart';
+// import 'package:stream_video_noise_cancellation/noise_cancellation_audio_processor.dart';
 import 'package:stream_video_push_notification/stream_video_push_notification.dart';
 
 import '../app/user_auth_controller.dart';
@@ -169,7 +169,7 @@ StreamVideo _initStreamVideo(
       muteAudioWhenInBackground: false,
       muteVideoWhenInBackground: false,
       keepConnectionsAliveWhenInBackground: true,
-      audioProcessor: NoiseCancellationAudioProcessor(),
+      // audioProcessor: NoiseCancellationAudioProcessor(),
     ),
     pushNotificationManagerProvider: StreamVideoPushNotificationManager.create(
       iosPushProvider: const StreamVideoPushProvider.apn(
