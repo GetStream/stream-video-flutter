cfa60c9cfec780fe38909fc1d566692c62eb63af
Brazol <brazol@gmail.com>
2025-12-30 16:42:04 +0100

    dependencies and proto update

--- packages/stream_video/lib/protobuf/video/sfu/models/models.pbenum.dart
+++ packages/stream_video/lib/protobuf/video/sfu/models/models.pbenum.dart
@@ -8,7 +8,7 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: curly_braces_in_flow_control_structures
 // ignore_for_file: deprecated_member_use_from_same_package, library_prefixes
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, prefer_relative_imports
 
 import 'dart:core' as $core;
 
@@ -539,13 +539,19 @@ class ClientCapability extends $pb.ProtobufEnum {
       ClientCapability._(
           1, _omitEnumNames ? '' : 'CLIENT_CAPABILITY_SUBSCRIBER_VIDEO_PAUSE');
 
+  /// Instructs SFU that stats will be sent to the coordinator
+  static const ClientCapability CLIENT_CAPABILITY_COORDINATOR_STATS =
+      ClientCapability._(
+          2, _omitEnumNames ? '' : 'CLIENT_CAPABILITY_COORDINATOR_STATS');
+
   static const $core.List<ClientCapability> values = <ClientCapability>[
     CLIENT_CAPABILITY_UNSPECIFIED,
     CLIENT_CAPABILITY_SUBSCRIBER_VIDEO_PAUSE,
+    CLIENT_CAPABILITY_COORDINATOR_STATS,
   ];
 
   static final $core.List<ClientCapability?> _byValue =
-      $pb.ProtobufEnum.$_initByValueList(values, 1);
+      $pb.ProtobufEnum.$_initByValueList(values, 2);
   static ClientCapability? valueOf($core.int value) =>
       value < 0 || value >= _byValue.length ? null : _byValue[value];
 
