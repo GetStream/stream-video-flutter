750dc46d7c5c67ef7c8a5a83c21f69cacb2fd627
Rene Floor <rene.floor@getstream.io>
2026-07-22 14:42:50 +0200

    UIScene delegate migration

--- packages/stream_video_flutter/example/ios/Runner/AppDelegate.swift
+++ packages/stream_video_flutter/example/ios/Runner/AppDelegate.swift
@@ -3,13 +3,16 @@ import UIKit
 import stream_video_push_notification
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
     StreamVideoPKDelegateManager.shared.registerForPushNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
